f3369c34565bceb4173cb3af1ba3870dc8fbe08d
Hayajiro <ciapa@lewd.wtf>
2023-10-28 00:13:17 +0200

    Add placeholder for help and credits buttons

--- scenes/mainmenu/buttons.gd
+++ scenes/mainmenu/buttons.gd
@@ -14,5 +14,15 @@ func _process(delta):
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
 
+
+func _on_help_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
+
+
 func _on_exit_button_pressed():
 	get_tree().quit()
+
