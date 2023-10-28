567497fbcc776ab01ce7aab178e68bb4a6f63611
Hayajiro <ciapa@lewd.wtf>
2023-10-28 15:26:52 +0200

    Properly progress mobs on path in sync with rotation

--- scenes/sprites/ghost/ghost.gd
+++ scenes/sprites/ghost/ghost.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
-@export var speed = 100
+@export var speed = 50
+
+var bg
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	bg = get_node("/root/level/StaircaseBackground")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_parent().set_progress(get_parent().get_progress() + speed * delta)
+	if bg.rotateNow:
+		get_parent().set_progress(get_parent().get_progress() + speed * delta * 3)
 	
 	if get_parent().get_progress_ratio() == 1:
 		var path = get_parent().get_parent()
