f40ce3f7ba95f47836eebf5f1b0d933e31232a4e
Bernd Stellwag <burned@zerties.org>
2023-10-28 20:26:59 +0200

    fix build script artifact naming

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ cp enable-threads.js dist/
 echo 'done.'
 
 echo 'Building for linux ...'
-godot --headless --no-window --export-release 'Linux/X11' dist/hack-to-the-root.x86_64
-chmod a+x dist/hack-to-the-root.x86_64
+godot --headless --no-window --export-release 'Linux/X11' dist/roundtrip.x86_64
+chmod a+x dist/roundtrip.x86_64
 echo 'done.'
 
 echo 'Building for Windows ...'
-godot --headless --no-window --export-release 'Windows Desktop' dist/hack-to-the-root.exe
+godot --headless --no-window --export-release 'Windows Desktop' dist/roundtrip.exe
 echo 'done.'
