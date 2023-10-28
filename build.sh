98bd98f18d17c480b24facf9f6cc584a02437288
Hayajiro <ciapa@lewd.wtf>
2023-10-28 11:46:09 +0200

    Fix headless build

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ rm -rf dist/
 mkdir -p dist
 
 echo 'Building for web ...'
-godot --no-window --export-release "HTML5" dist/index.html
+godot --headless --no-window --export-release "HTML5" dist/index.html
 cp enable-threads.js dist/
 echo 'done.'
 
 echo 'Building for linux ...'
-godot --no-window --export-release 'Linux/X11' dist/hack-to-the-root.x86_64
+godot --headless --no-window --export-release 'Linux/X11' dist/hack-to-the-root.x86_64
 chmod a+x dist/hack-to-the-root.x86_64
 echo 'done.'
 
 echo 'Building for Windows ...'
-godot --no-window --export-release 'Windows Desktop' dist/hack-to-the-root.exe
+godot --headless --no-window --export-release 'Windows Desktop' dist/hack-to-the-root.exe
 echo 'done.'
