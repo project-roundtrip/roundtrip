8bc48e6178519c1531b97b66820e6b3e3918afb3
Hayajiro <ciapa@lewd.wtf>
2023-10-28 11:40:10 +0200

    Add basics for GH Pages deployment

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf dist/
+mkdir -p dist
+
+echo 'Building for web ...'
+godot4 --no-window --export-release "HTML5" dist/index.html
+cp enable-threads.js dist/
+echo 'done.'
+
+echo 'Building for linux ...'
+godot4 --no-window --export-release 'Linux/X11' dist/hack-to-the-root.x86_64
+chmod a+x dist/hack-to-the-root.x86_64
+echo 'done.'
+
+echo 'Building for Windows ...'
+godot4 --no-window --export-release 'Windows Desktop' dist/hack-to-the-root.exe
+echo 'done.'
