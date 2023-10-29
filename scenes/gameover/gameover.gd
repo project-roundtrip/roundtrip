3ff0d382d691dae0a9772fff6225b64d13b947dd
Bernd Stellwag <burned@zerties.org>
2023-10-29 13:23:06 +0100

    make esc go to main menu from game over screen

--- scenes/gameover/gameover.gd
+++ scenes/gameover/gameover.gd
@@ -8,4 +8,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if Input.is_action_just_pressed("exit"):
+		get_tree().change_scene_to_file("res://scenes/mainmenu/mainmenu.tscn")
