ca65b8dc33cfe865e43a61a978e475c67f4377a8
EduardBaer <EduardBaer@users.noreply.github.com>
2023-10-29 13:09:42 +0100

    Make game over scene controller controllable

--- scenes/gameover/ControlsContainer.gd
+++ scenes/gameover/ControlsContainer.gd
@@ -1,9 +1,10 @@
 extends VBoxContainer
 
+var first_focus = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$TryAgain.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +16,15 @@ func _on_try_again_pressed():
 	
 func _on_BackToMainMenu_pressed():
 	get_tree().change_scene_to_file("res://scenes/mainmenu/mainmenu.tscn")
+
+func _on_button_focus_entered():
+	if first_focus:
+		first_focus = false
+	else:
+		$click.play()
+
+func _on_try_again_mouse_entered():
+	$TryAgain.grab_focus()
+
+func _on_back_to_main_menu_mouse_entered():
+	$BackToMainMenu.grab_focus()
